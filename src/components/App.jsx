1579bc79b5a69a761ae30034e83136133ea33d8a
Anna Zhezhera <ianna22121996@gmail.com>
2022-11-07 12:15:46 +0200

    test 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import React, { useState } from 'react';
+import React from 'react';
 import { Layout } from './Layout/Layout';
 import { AppWrapp } from './App.styled';
 import Home from 'pages/Home';
